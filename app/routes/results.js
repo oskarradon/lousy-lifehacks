edf160281c3d6bee1e4cdc25e5fa39afb0de786c
andrew <Guest@epicodus-9.local>
2016-04-13 14:23:46 -0700

    Add search component to results route

--- app/routes/results.js
+++ app/routes/results.js
@@ -2,19 +2,16 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function(params) {
-    var url = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&order=date&q=" + params.query + "how+to&type=video&videoCaption=closedCaption&key=AIzaSyARsAKkRYUFn5_CsH19dR-kfJf9foLW0i4";
+    var url = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=3&order=date&q=" + params.query + "life+hack&key=AIzaSyARsAKkRYUFn5_CsH19dR-kfJf9foLW0i4";
     return Ember.$.getJSON(url).then(function(responseJSON) {
       console.log(responseJSON.items[0].id.videoId);
       return responseJSON.items;
     });
   },
-  afterModel: function() {
-
-      // 3. This function creates an <iframe> (and YouTube player)
-      //    after the API code downloads.
-    },
 
   actions: {
-
+    lookupForm(params) {
+      this.transitionTo('results', params.query);
+    },
   }
 });
