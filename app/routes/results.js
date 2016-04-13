645c029b4f229a23af8c6d3469a656c660dc4d4c
andrew <Guest@epicodus-9.local>
2016-04-13 11:35:26 -0700

    JSON that returns the videoId

--- app/routes/results.js
+++ app/routes/results.js
@@ -5,12 +5,12 @@ export default Ember.Route.extend({
     var url = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&q="
     + params.query + "&type=video&videoCaption=closedCaption&key=AIzaSyARsAKkRYUFn5_CsH19dR-kfJf9foLW0i4";
     return Ember.$.getJSON(url).then(function(responseJSON) {
-      console.log(responseJSON);
-      return responseJSON.results;
+      console.log(responseJSON.items[0].id.videoId);
+      return responseJSON.items;
     });
   },
 
   actions: {
-    
+
   }
 });
