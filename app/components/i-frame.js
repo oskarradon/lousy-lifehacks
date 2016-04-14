5f65f40322ce53eb979c06a0aaccdfe551e446e3
Oskar Radon <oskarradonkimball@gmail.com>
2016-04-14 14:33:01 -0700

    👽 Link to the new partials

--- app/components/i-frame.js
+++ app/components/i-frame.js
@@ -2,7 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   favoriteVideo: Ember.inject.service('favorite-video'),
-
   actions: {
     addFavorite(favorite) {
       console.log(this.get('favoriteVideo'));
