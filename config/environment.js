f28c5d1f9ca1101a111baf827fdbc8f5771dd20d
keir staple and trevor elvey <keirstaple@comcast.net>
2016-04-14 10:04:49 -0700

    add user and favorite models and install emberfire and torii

--- config/environment.js
+++ config/environment.js
@@ -4,6 +4,11 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'crappy-lifehacks',
     environment: environment,
+    contentSecurityPolicy: { 'connect-src': "'self' https://auth.firebase.com wss://*.firebaseio.com" },
+    firebase: 'https://lousy-lifehacks.firebaseio.com/',
+    torii: {
+      sessionServiceName: 'session'
+    },
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
