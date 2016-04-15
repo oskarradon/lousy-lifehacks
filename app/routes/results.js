66def4ca71b80bc19783b1010f5fd6e7975e3601
Oskar Radon <oskarradonkimball@gmail.com>
2016-04-15 10:16:12 -0700

    merge

--- app/routes/results.js
+++ app/routes/results.js
@@ -5,7 +5,7 @@ const { inject } = Ember;
 export default Ember.Route.extend({
   authenticate: Ember.inject.service('authenticate'),
   model: function(params) {
-    var url = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=3&order=date&q=" + params.query + "how+to+lifehack&key=AIzaSyARsAKkRYUFn5_CsH19dR-kfJf9foLW0i4";
+    var url = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=3&order=date&q=" + params.query + "lifehack+how+to&key=AIzaSyARsAKkRYUFn5_CsH19dR-kfJf9foLW0i4";
     return Ember.$.getJSON(url).then(function(responseJSON) {
       if (responseJSON.items.length < 1) {
         alert('Try a new search term');
