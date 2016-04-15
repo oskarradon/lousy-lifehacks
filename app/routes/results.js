7395cf539b0b1b5ebc565b6c33b464b9999f3380
Oskar Radon <oskarradonkimball@gmail.com>
2016-04-15 08:31:16 -0700

    👽 Make app actually search for life hack videos lol

--- app/routes/results.js
+++ app/routes/results.js
@@ -5,7 +5,7 @@ const { inject } = Ember;
 export default Ember.Route.extend({
   authenticate: Ember.inject.service('authenticate'),
   model: function(params) {
-    var url = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=3&order=date&q=" + params.query + "&key=AIzaSyARsAKkRYUFn5_CsH19dR-kfJf9foLW0i4";
+    var url = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=3&order=date&q=" + params.query + "lifehack&key=AIzaSyARsAKkRYUFn5_CsH19dR-kfJf9foLW0i4";
     return Ember.$.getJSON(url).then(function(responseJSON) {
       console.log(responseJSON.items[0].id.videoId);
       return responseJSON.items;
